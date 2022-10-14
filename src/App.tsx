bb1bd8aa8dafdaa34e7fe4f50f07d129cc7f8606
alicematins7 <martinsalice280206@gmail.com>
2022-10-14 10:41:53 -0300

    a

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import GlobalStyle from "styles/GlobalStyle";
+import Routes from "routes";
+import { ToastContainer } from "react-toastify";
+import { AuthProvider } from "contexts/authContext";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <ToastContainer autoClose={3000} />
+      <AuthProvider>
+      <Routes />
+      </AuthProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Adm/Index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./styles";
+
+const Adm = () => {
+    return (
+        <S.adm>
+            Área administrativa
+        </S.adm>
+    );
+};
+
+export default Adm;
